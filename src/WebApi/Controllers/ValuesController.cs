e6f82977565cb9c79d35f0d2117f49b302d64df8
Cort Schaefer <cort@eschaefer.net>
2025-01-29 12:34:14 -0700

    add example to show the difference in request models between required keywork and required attribute

---
+++ src/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using WeatherForecast.WebApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WeatherForecast.WebApi.Controllers {
+    [ApiController]
+    [ApiVersion("1")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/values")]
+    public class ValuesController : ControllerBase {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Value> Get() {
+            return new[] { new Value() { Id = 1, Name = "value", Description = "description" }, new Value() { Id = 2, Name = "value", Description = "description" } };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id) {
+            return Ok(new Value() { Id = id, Name = "value", Description = "description" });
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Value value) {
+            return Ok(value);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Value value) {
+            return Ok(value);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) {
+            return NoContent();
+        }
+    }
+}
---
+++ src/WebApi/Models/Value.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherForecast.WebApi.Models {
+    public class Value {
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+    }
+}
